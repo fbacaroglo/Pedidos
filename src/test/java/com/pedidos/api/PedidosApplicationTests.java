6b91fca8551eac38dc661fbe44e8ec07fb64e060
Fabio Eduardo Bacaroglo <fabio.bacaroglo@tivit.com>
2020-12-27 12:08:29 -0300

    Arquivos Iniciais

---
+++ src/test/java/com/pedidos/api/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedidos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
